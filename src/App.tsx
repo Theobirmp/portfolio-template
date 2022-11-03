c18ec9db98006486e799facc7f44638e7a9c323d
Theobirmp <theobirmpil@gmail.com>
2022-11-03 19:06:31 +0200

    redirect to home page when wrong link

--- src/App.tsx
+++ src/App.tsx
@@ -37,6 +37,7 @@ function App() {
               <Route path="/contact" element={<Contact />} />
               <Route path="/services" element={<Services />} />
               <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="*" element={<Home />} />
             </Routes>
           </AnimatePresence>
         </AppContainer>
